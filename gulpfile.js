5a0532089223cddfc588231324ef0ccd6512c2ae
Salieri36 <36salieri@gmail.com>
2020-08-06 22:02:31 +0300

    add compress css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
  
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./css'));
 });
  
